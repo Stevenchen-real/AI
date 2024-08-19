349117ef0bb94f1f6b667b02475e0e38dfbabb86
Steven__Chen <1049273544@qq.com>
2024-08-19 19:02:13 +0800

    test

---
+++ model.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+# 加载MNIST数据集
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+
+# 标准化图像数据
+train_images = train_images.reshape((60000, 28, 28, 1)).astype('float32') / 255
+test_images = test_images.reshape((10000, 28, 28, 1)).astype('float32') / 255
+
+# 将标签转换为one-hot编码
+train_labels = tf.keras.utils.to_categorical(train_labels)
+test_labels = tf.keras.utils.to_categorical(test_labels)
+
+# 创建Sequential模型
+model = models.Sequential()
+
+# 添加卷积层
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+
+# 添加Flatten层
+model.add(layers.Flatten())
+
+# 添加全连接层
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(64, activation='sigmoid'))
+model.add(layers.Dropout(0.2))
+model.add(layers.Dense(10, activation='softmax'))
+
+# 编译模型
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# 训练模型
+model.fit(train_images, train_labels, epochs=5, batch_size=64)
+
+# 评估模型
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f'测试精度: {test_acc:.4f}')
---
+++ test.py
@@ -0,0 +1,2 @@
+import tensorflow as tf
+x = tf.random.uniform([10000,10000])
